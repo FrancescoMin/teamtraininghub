40dc445c9fbd77c77a7f6bbdbba67646063c417f
FrancescoMin <francescominotti2001@gmail.com>
2023-12-14 14:36:43 +0100

    Inizio Progetto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.framin.teamtraininghub {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.framin.teamtraininghub to javafx.fxml;
+    exports com.framin.teamtraininghub;
+}
